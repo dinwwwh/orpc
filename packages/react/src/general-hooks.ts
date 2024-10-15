e09f3ab0cacc891ee1760fd1bf50ba807801b58d
dinwwwh <dinwwwh@gmail.com>
2024-10-15 08:53:38 +0700

    general hooks

--- packages/react/src/general-hooks.ts
+++ packages/react/src/general-hooks.ts
@@ -1,12 +1,50 @@
-import type { Schema, SchemaOutput } from '@orpc/contract'
-import type { ORPCContext } from './react-context'
+import type { Schema, SchemaInput, SchemaOutput } from '@orpc/contract'
+import {
+  type Mutation,
+  type MutationFilters,
+  type MutationState,
+  type OmitKeyof,
+  type QueryFilters,
+  useIsFetching,
+  useIsMutating,
+  useMutationState,
+} from '@tanstack/react-query'
+import {
+  type QueryType,
+  getMutationKeyFromPath,
+  getQueryKeyFromPath,
+} from './key'
+import { type ORPCContext, useORPCContext } from './react-context'
 
 export interface GeneralHooks<
   TInputSchema extends Schema,
   TOutputSchema extends Schema,
   THandlerOutput extends SchemaOutput<TOutputSchema>,
 > {
-  useIsMutating: any
+  useIsFetching: (
+    input?: SchemaInput<TInputSchema>,
+    filers?: OmitKeyof<QueryFilters, 'queryKey'> & { type?: QueryType },
+  ) => number
+  useIsMutating: (filters?: OmitKeyof<MutationFilters, 'mutationKey'>) => number
+
+  useMutationState: <
+    UResult = MutationState<
+      SchemaOutput<TOutputSchema, THandlerOutput>,
+      unknown,
+      SchemaInput<TInputSchema>,
+      unknown
+    >,
+  >(options?: {
+    filters?: OmitKeyof<MutationFilters, 'mutationKey'>
+    select?: (
+      mutation: Mutation<
+        SchemaOutput<TOutputSchema, THandlerOutput>,
+        unknown,
+        SchemaInput<TInputSchema>,
+        unknown
+      >,
+    ) => UResult
+  }) => UResult[]
 }
 
 export interface CreateGeneralHooksOptions {
@@ -27,6 +65,38 @@ export function createGeneralHooks<
 >(
   options: CreateGeneralHooksOptions,
 ): GeneralHooks<TInputSchema, TOutputSchema, THandlerOutput> {
-  // TODO
-  return {} as any
+  return {
+    useIsFetching(input, filters) {
+      const { type, ...rest } = filters ?? {}
+      const context = useORPCContext(options.context)
+      return useIsFetching(
+        {
+          queryKey: getQueryKeyFromPath(options.path, { input, type }),
+          ...rest,
+        },
+        context.queryClient,
+      )
+    },
+    useIsMutating(filters) {
+      const context = useORPCContext(options.context)
+      return useIsMutating(
+        { mutationKey: getMutationKeyFromPath(options.path), ...filters },
+        context.queryClient,
+      )
+    },
+
+    useMutationState(options_) {
+      const context = useORPCContext(options.context)
+      return useMutationState(
+        {
+          ...(options_ as any),
+          filters: {
+            mutationKey: getMutationKeyFromPath(options.path),
+            ...options_?.filters,
+          },
+        },
+        context.queryClient,
+      )
+    },
+  }
 }
